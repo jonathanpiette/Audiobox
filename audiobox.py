19d071443adca973cb4f6167ed3f9ac762a42042
Jonathan Piette <jo.piette@me.com>
2023-08-30 16:01:57 +0200

    wip working version

--- audiobox.py
+++ audiobox.py
@@ -6,20 +6,32 @@ from adafruit_pn532.i2c import PN532_I2C
 import pygame
 import threading
 import time
+#import RPi.GPIO as GPIO
 
-# Set the working directory to the script's directory
+# Configuration
+#CLK_PIN = 18
+#DT_PIN = 17
+#SW_PIN = 27
+
+# Initialize GPIO
+#GPIO.setmode(GPIO.BCM)
+#GPIO.setup([CLK_PIN, DT_PIN, SW_PIN], GPIO.IN, pull_up_down=GPIO.PUD_UP)
+#print("GPIO pins configured.")
+
+# Set working directory
 abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
 os.chdir(dname)
 
-# Créer une instance de la classe PN532 en utilisant I2C
+# Initialize PN532
 i2c = board.I2C()
 pn532 = PN532_I2C(i2c)
+print("PN532 initialized.")
 
-# Initialiser la communication avec le PN532
+# Configure PN532
 pn532.SAM_configuration()
 
-# Charger la liste des musiques associées aux tags NFC à partir du fichier audio_map.txt
+# Load audio map
 def load_audio_map():
     audio_map = {}
     try:
@@ -33,56 +45,99 @@ def load_audio_map():
         print("Le fichier sera créé lors de l'association de tags NFC avec des fichiers audio.")
     return audio_map
 
-# Charger la liste des musiques associées aux tags NFC
-nfc_audio_map = load_audio_map()
-
 # Enregistrer la liste des musiques associées aux tags NFC dans le fichier audio_map.txt
 def save_audio_map(audio_map):
     with open('audio_map.txt', 'w') as file:
         for tag_id, audio_file in audio_map.items():
             file.write(f"{tag_id},{audio_file}\n")
 
-# Fonction pour initialiser Pygame mixer
+# Charger la liste des musiques associées aux tags NFC
+nfc_audio_map = load_audio_map()
+print("Audio map loaded.")
+
+# Initialize Pygame mixer
 def initialize_pygame_mixer():
     pygame.mixer.init()
+#    pygame.mixer.music.set_volume(1)  # Set default volume
+    
+#last_CLK_state = GPIO.input(CLK_PIN)
 
-# Fonction pour jouer le fichier audio
+# Play audio
 def play_audio(file_path):
     music_folder = os.path.join(dname, 'musics')
     audio_file_path = os.path.join(music_folder, file_path)
+    print("Playing audio:", audio_file_path)
     pygame.mixer.music.load(audio_file_path)
     pygame.mixer.music.play()
 
-# Fonction pour arrêter la lecture audio
+# Stop audio
 def stop_audio():
     pygame.mixer.music.stop()
 
-# Fonction pour scanner un tag NFC
+# Volume control
+#def volume_control(channel):
+#    global last_CLK_state
+#    CLK_state = GPIO.input(CLK_PIN)
+#    DT_state = GPIO.input(DT_PIN)
+#    print("Volume control event detected.")
+#    print("CLK:", CLK_state)
+#    print("DT:", DT_state)
+#    if CLK_state != last_CLK_state:
+#        if DT_state != CLK_state:
+#            pygame.mixer.music.set_volume(min(pygame.mixer.music.get_volume() + 0.1, 1.0))
+#            print("Volume increased:", pygame.mixer.music.get_volume())
+#        else:
+#            pygame.mixer.music.set_volume(max(pygame.mixer.music.get_volume() - 0.1, 0.0))
+#            print("Volume decreased:", pygame.mixer.music.get_volume())
+#    last_CLK_state = CLK_state
+
+# Set up switch pin
+#GPIO.setup(SW_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+#print("Switch pin configured.")
+
+# Switch press event
+#def switch_pressed(channel):
+#    print("Switch pressed!")
+
+# Remove existing event detection for the switch pin
+#GPIO.remove_event_detect(SW_PIN)
+
+# Attach switch press event
+#GPIO.add_event_detect(SW_PIN, GPIO.FALLING, callback=switch_pressed, bouncetime=300)
+#print("Switch event attached to pin.")
+
+# Attach volume control event
+#GPIO.add_event_detect(CLK_PIN, GPIO.FALLING, callback=volume_control, bouncetime=50)
+#print("Volume control attached to pins.")
+
+# NFC tag scanning
 def scan_nfc_tag():
     try:
         tag_id = pn532.read_passive_target(timeout=0.5)
         if tag_id is not None:
-            return bytes(tag_id).hex()  # Convert the bytearray to a hex string
+            # Convert the bytearray to a hex string
+            return bytes(tag_id).hex()
     except RuntimeError as e:
         print("Erreur lors de la lecture du tag NFC:", e)
         print("Assurez-vous que le module PN532 est correctement connecté et que l'étiquette NFC est à proximité.")
         return None
 
-# Vérifier si un fichier est un fichier MP3
+# Check if a file is an MP3 file
 def is_mp3_file(filename):
     return filename.lower().endswith('.mp3')
 
-# Liste des musiques dans le dossier 'musics'
+# Music files
 music_folder = os.path.join(dname, 'musics')
 music_files = [file for file in os.listdir(music_folder) if is_mp3_file(file)]
 
-# Variable pour suivre le dernier tag scanné et le temps où il a été scanné
+# Variables for tracking the last scanned tag and time
 last_tag_id = None
 last_tag_time = 0
 
+# NFC thread
 def check_nfc_thread():
     global last_tag_id, last_tag_time
-
+    
     # Initialiser Pygame mixer avant de démarrer le thread
     initialize_pygame_mixer()
 
@@ -121,3 +176,4 @@ except KeyboardInterrupt:
     nfc_thread.join()
     # Enregistrer la liste des musiques associées aux tags NFC dans le fichier audio_map.txt avant de quitter
     save_audio_map(nfc_audio_map)
+#    GPIO.cleanup()
