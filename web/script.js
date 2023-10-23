c94c14c7174b09d3a6874cd4be84f3f3889952fa
Jonathan Piette <jo.piette@me.com>
2023-10-23 23:33:28 +0200

    wip list

--- web/script.js
+++ web/script.js
@@ -1,49 +1,51 @@
 function browseForFile() {
     const fileInput = document.getElementById("file-upload");
+    const fileUpload = document.getElementById('file-upload');
+
     fileInput.click(); // Trigger the file input click event when the "Browse" button is clicked.
+    // fileUpload.click(); // Trigger the file input click event when the "Browse" button is clicked.
+
     fileInput.addEventListener("change", function () {
+        const fileList = fileUpload.files;
+        const fileListContainer = document.getElementById('file-list');
+        // Clear the previous list
+        fileListContainer.innerHTML = '';
         if (fileInput.files.length > 0) {
             const file = fileInput.files[0];
             document.getElementById("file-input").value = file.name;
-            document.getElementById("file-icon").style.display = "inline"; // Show the file icon.
+            
+        }
+
+        for (const file of fileList) {
+            const fileIcon = document.createElement('img');
+            const cancelIcon = document.createElement('img');
+            const listItem = document.createElement('li');
+            const listContainer = document.createElement('div'); // Create a container for the elements
+
+
+            fileIcon.classList.add('file-icon');
+            cancelIcon.classList.add('cancel-icon');
+            listContainer.classList.add('list-container')
+            
+            fileIcon.src = 'file-icon.png';
+            cancelIcon.src = 'cancel-icon.png';
+
+            listItem.textContent = file.name;
+
+            // Add the elements to the container
+            listContainer.appendChild(fileIcon);
+            listContainer.appendChild(listItem);
+            listContainer.appendChild(cancelIcon);
+
+            fileListContainer.appendChild(listContainer);
         }
     });
 }
 
-const fileInput = document.getElementById("file-input");
-const fileIcon = document.getElementById("file-icon");
-
-fileInput.addEventListener("dragover", (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-});
-
-fileInput.addEventListener("dragenter", () => {
-    fileInput.classList.add("dragover");
-});
-
-fileInput.addEventListener("dragleave", () => {
-    fileInput.classList.remove("dragover");
-});
-
-fileInput.addEventListener("drop", (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    fileInput.classList.remove("dragover");
-
-    const files = e.dataTransfer.files;
-    if (files.length > 0) {
-        const file = files[0];
-        fileInput.value = file.name;
-        fileIcon.style.display = "inline"; // Show the file icon.
-    }
-});
-
-// Listen for changes to the file input
-fileInput.addEventListener("change", () => {
-    const file = fileInput.files[0];
-    if (file) {
-        fileIcon.style.display = "inline"; // Show the file icon.
-    }
-});
+// Add event listener for the cancel icon
+// cancelIcon.addEventListener("click", () => {
+//     fileInput.value = ''; // Clear the file input field
+//     // cancelIcon.style.display = 'none'; // Hide the cancel icon
+//     // fileIcon.style.display = 'none'; // Hide the file icon
+// });
 
